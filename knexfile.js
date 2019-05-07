d2260fc11f2239831340770720a61c04440a5c42
Vision-X <michael.s.marlow@gmail.com>
2019-05-06 21:19:39 -0600

    new db

--- knexfile.js
+++ knexfile.js
@@ -4,17 +4,17 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgresql:///locations-db'
+    connection: 'postgresql:///locations-database'
   },
 
   test: {
     client: 'pg',
-    connection: 'postgresql:///locations-db'
+    connection: 'postgresql:///locations-database'
   },
 
   production: {
     client: 'pg',
-    connection: `${process.env.DATABASE_URL}?ssl=true` || 'postgres://localhost/locations-db',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     pool: {
       min: 2,
       max: 10
