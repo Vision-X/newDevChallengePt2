a7c13c6e0bb8bbb1ec5624f394f52f2b81fa0645
Vision-X <michael.s.marlow@gmail.com>
2019-05-06 21:34:12 -0600

    heroku debugging

--- seeds/locations_seed.js
+++ seeds/locations_seed.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('locations').del()
-    .then(function () {
+  // return knex('locations').del()
+    // .then(function () {
       // Inserts seed entries
       return Promise.all([
          knex('locations').insert([
